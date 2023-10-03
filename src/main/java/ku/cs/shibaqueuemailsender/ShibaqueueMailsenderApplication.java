7062943fe8e050f044d88f6b7caf56ed0f26fe0f
kankittikan <nongkan195@gmail.com>
2023-10-03 19:19:43 +0700

    first commit

---
+++ src/main/java/ku/cs/shibaqueuemailsender/ShibaqueueMailsenderApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.shibaqueuemailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShibaqueueMailsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShibaqueueMailsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/shibaqueuemailsender/ShibaqueueMailsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.shibaqueuemailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShibaqueueMailsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
